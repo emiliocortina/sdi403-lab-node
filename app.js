211bc5672745f64dffb44b022e8cb81232576335
emilio <emiliocortina@gmail.com>
2019-03-19 17:59:00 +0100

    SDI-NodeJS-.3,4,5 Express, depuración y varibles de entorno

---
+++ app.js
@@ -0,0 +1,20 @@
+//Modulos
+var express = require('express');
+var app = express();
+
+// Variables
+app.set('port', 8081);
+
+app.get('/usuarios', function(req, res) {
+    console.log("Depurar aquó");
+    res.send('ver usuarios');
+});
+
+app.get('/canciones', function(req, res) {
+    res.send('ver canciones');
+});
+
+// lanzarel servidor
+app.listen(app.get('port'), function() {
+    console.log("Servidor activo");
+});
