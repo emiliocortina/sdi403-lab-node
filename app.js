46d7180930df766ac06a51b35186894187a35440
emilio <emiliocortina@gmail.com>
2019-03-26 20:22:16 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ var swig = require('swig');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static('public'));
-
 var mongo = require('mongodb');
-
 
 // Variables
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00-ri6vu.mongodb.net:27017,tiendamusica-shard-00-01-ri6vu.mongodb.net:27017,tiendamusica-shard-00-02-ri6vu.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
 
-require("./routes/rusuarios")(app, swig, mongo);
-require("./routes/rcanciones.js")(app, swig, mongo);
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+require("./routes/rusuarios")(app, swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD);
 
 // lanzarel servidor
 app.listen(app.get('port'), function() {
